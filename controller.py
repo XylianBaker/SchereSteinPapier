37d63bd9f8e4f857648143b16592da7d4f683320
Jan Kammellander <janschwiderik@gmail.com>
2022-10-30 21:08:49 +0100

    added the controller for the game

---
+++ controller.py
@@ -0,0 +1,44 @@
+# Controller for the qt gui for the rock paper scissors game
+
+import sys
+from PyQt6.QtWidgets import QApplication
+import model
+import view
+
+
+class Controller:
+    def __init__(self):
+        self.model = model.Game()
+        self.view = view.View(self)
+        self.view.show()
+
+    def execute(self):
+        self.model.set_player_move(self.view.get_player_move())
+        self.model.set_computer_move(self.model.get_computer_move())
+        self.view.set_text_player_move(self.model.get_player_move())
+        self.view.set_text_computer_move(self.model.get_computer_move())
+        if self.model.is_tie():
+            self.view.set_text_statusbar('Unentschieden')
+        elif self.model.is_player_winner():
+            self.model.set_player_score(self.model.get_player_score() + 1)
+            self.view.set_text_player_score(self.model.get_player_score())
+            self.view.set_text_statusbar('Spieler gewinnt')
+        else:
+            self.model.set_computer_score(self.model.get_computer_score() + 1)
+            self.view.set_text_computer_score(self.model.get_computer_score())
+            self.view.set_text_statusbar('Computer gewinnt')
+        self.model.set_round(self.model.get_round() + 1)
+        self.view.set_text_round(self.model.get_round())
+
+    def reset(self):
+        self.model.set_round(1)
+        self.model.set_player_score(0)
+        self.model.set_computer_score(0)
+        self.view.reset()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    c = Controller()
+    c.view.show()
+    sys.exit(app.exec())
--- view.py
+++ view.py
@@ -3,16 +3,21 @@
 from PyQt6.QtWidgets import *
 from PyQt6 import uic
 
+from controller import Controller
+
 
 class View(QMainWindow):
+    move: QComboBox
+
     def __init__(self, c: Controller):
         super().__init__()
-        uic.loadUI("gui.ui", self)
+        self.setWindowTitle("Rock Paper Scissors")
+        uic.loadUi("gui.ui", self)
         self.move.addItems(['rock', 'paper', 'scissors'])
         self.pB_execute.clicked.connect(c.execute)
         self.pB_reset.clicked.connect(c.reset)
 
-    def reset(self)->None:
+    def reset(self) -> None:
         self.move.setCurrentIndex(0)
         self.round.setText('1')
         self.player_score.setText('0')
@@ -21,17 +26,17 @@ class View(QMainWindow):
         self.computer_move.setText('Spielzug des Computers')
         self.set_text_statusbar('Sarte mit dem ersten Spielzug')
 
-    def set_text_statusbar(self, text: str)->None:
+    def set_text_statusbar(self, text: str) -> None:
         self.statusbar.showMessage(text)
 
-    def set_text_round(self, text: str)->None:
+    def set_text_round(self, text: str) -> None:
         self.round.setText(text)
 
-    def set_text_player_score(self, text: str)->None:
+    def set_text_player_score(self, text: str) -> None:
         self.player_score.setText(text)
 
-    def set_text_computer_score(self, text: str)->None:
+    def set_text_computer_score(self, text: str) -> None:
         self.computer_score.setText(text)
 
-    def get_player_move(self)->str:
+    def get_player_move(self) -> str:
         return self.move.currentText()
